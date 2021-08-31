7e689502fb526f60e2d8852f7f01908c9668fc9a
Fredrik Ridderfalk <ridderfalk@gmail.com>
2021-08-31 17:33:42 +0200

    initial commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// Every object in JavaScript has a property called constructor, and that references the function that was used to construct or create that object.
+
+// Factory Function (my preferred way of creating an object me thinks). What makes it a factory function is that something is returned inside the function.
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function () {
+      console.log("draw");
+    },
+  };
+}
+
+const circle = createCircle(1);
+
+// Constructor Function (the preferred way of creating an object for people coming from C++ and Java cause it looks similar to creating a class)
+function Circle(radius) {
+  console.log("this", this);
+  this.radius = radius;
+  this.draw = function () {
+    console.log("draw");
+  };
+}
+const another = new Circle(1);
